3568735514ad50e33b22caa8bb86444c74a0f66d
Cameron <cholbrook582@gmail.com>
2022-07-31 20:15:41 -0500

    Chose only publishedAt
    There are minor differences between YouTube's actualStartTime,
    videoPublished, and scheduledStartTime. They're all basically the same.
    And because every video has a publishedAt attribute, I will use this.
    
    Also, I found out that publishedAt dates are 5 hours later than the
    actual date? I still don't know why, but based on my testing, this 5
    hour difference is reliable. So, this information can be used for
    further fine-tuning of podcast episode detection, by selecting specific
    dates for YouTube podcasts, instead of only videos longer than an
    arbitrary length of time.

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def get_podcast_ids(api_key, podcast_channels):
         print("podcast_channels file is empty. Returning... ")
         return
 
-    cumulative = []
+    cumulative_video_info = []
     video_ids = []
     max_returns = 25            # Using a separate variable for video id extraction below.
 
@@ -62,7 +62,7 @@ def get_podcast_ids(api_key, podcast_channels):
 
         # Execute API call for each channel.
         response = request.execute()        # response is a dictionary
-        cumulative.append(response)
+        cumulative_video_info.append(response)
 
 
     # 2. Extract the video_ids.
@@ -70,9 +70,9 @@ def get_podcast_ids(api_key, podcast_channels):
     # This code is strange, and I'm not completely settled on it.
     # Basically, it's a nested for loop to iterate through a YouTube channel's videos for EACH channel.
     # Outer is for each channel, inner is for each video in that one channel.
-    for x in range(0, len(cumulative)):
+    for x in range(0, len(cumulative_video_info)):
         for y in range(0, max_returns):
-            video_ids.append(cumulative[x]['items'][y]['contentDetails']['videoId'])
+            video_ids.append(cumulative_video_info[x]['items'][y]['contentDetails']['videoId'])
 
 
     return video_ids
@@ -84,13 +84,14 @@ def determine_valid_podcasts(api_key, podcast_urls):
     valid_publish_datetime = False
     valid_duration = False
 
-    checkpoint = datetime.datetime(2022, 7, 20, 8, 0, 0)
-    minimum_duration = datetime.timedelta(days=0, minutes=2, seconds=0)
+    checkpoint = datetime.datetime(2022, 7, 24, 8, 0, 0)
+    minimum_duration = datetime.timedelta(days=0, minutes=30, seconds=0)
 
     # Here, I have just a list of id's; that's it. I've no way to differentiate it.
     # To distinguish podcasts:
     # Greater than 45 minutes long.             # items, contentDetails, duration
     # Published AFTER checkpoint datetime.       # items, snippet, publishedAt
+    # Published +- 30 minutes around scheduled upload time.
     # Other: items, id
     youtube_service = build('youtube', 'v3', developerKey=api_key)
 
@@ -98,31 +99,38 @@ def determine_valid_podcasts(api_key, podcast_urls):
 
         # Build YouTube object, and execute it.
         request = youtube_service.videos().list(
-            part="snippet, contentDetails",
+            part="snippet, contentDetails, liveStreamingDetails",
             id=podcast_id
         )
 
         response = request.execute()
 
+        # TESTING
 
-        # If duration is greater than 45 minutes, set valid_duration to True
+
+
+        ############################################# Podcast Checks #############################################
+
+        # Duration check
         duration = isodate.parse_duration(response['items'][0]['contentDetails']['duration'])
 
         if duration >= minimum_duration:
             valid_duration = True
-        else:
-            valid_duration = False
 
-        # Code that compares video publish date to checkpoint.
-        video_publishedAt = response['items'][0]['snippet']['publishedAt']
-        video_publishedAt = datetime.datetime.strptime(video_publishedAt, "%Y-%m-%dT%H:%M:%SZ")
 
+
+        # Checkpoint check
+        video_publishedAt = response['items'][0]['snippet']['publishedAt']                          # Return published datetime.
+        video_publishedAt = datetime.datetime.strptime(video_publishedAt, "%Y-%m-%dT%H:%M:%SZ")     # Convert to string.
 
         if video_publishedAt > checkpoint:
             valid_publish_datetime = True
-        else:
-            valid_publish_datetime = False
 
+
+
+
+
+        ############################################# Final Checks #############################################
 
         # Determine if video is a valid podcast.
         if valid_duration and valid_publish_datetime:
@@ -154,7 +162,7 @@ def derive_podcast_urls(podcast_ids):
     return podcast_urls
 
 
-def create_checkpoint():
+def update_checkpoint():
 
     datetime_now = datetime.datetime.now()
 
@@ -201,5 +209,21 @@ if __name__ == "__main__":
 #
 # test_list = [video_urls, video_published_ats]
 # print(test_list[0])
+
+
+# print("video published:", response['items'][0]['snippet']['publishedAt'], end=". ")
+#
+# try:
+#     print("actualStartTime:", response['items'][0]['liveStreamingDetails']['actualStartTime'], end=" ")
+# except Exception:
+#     print("Eh, an actual error happened.", end=" ")
+#
+# try:
+#     print("scheduleStartTime:", response['items'][0]['liveStreamingDetails']['scheduledStartTime'], end=" ")
+# except Exception:
+#     print("Eh, a schedule error happened.", end=" ")
+#
+# print()
+
 
 ################ TESTING
